2e23241debfe314eba9838be97c6d674464e5128
Agustín Roca <aroca@itba.edu.ar>
2021-03-24 19:38:01 -0300

    Center labels

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from dataparser import *
 from matplotlib.colors import ListedColormap
+import numpy as np
 
 simdata = data_parser("../data/output.txt")
 
@@ -33,7 +34,7 @@ if simdata.sim_type == "3D":
     
     plt.show()
 else:
-    mat = plt.matshow(simdata.frames[0].mat, extent=[0, simdata.sim_size[0], 0, simdata.sim_size[1]], cmap=ListedColormap(['w', 'k']))
+    mat = plt.matshow(simdata.frames[0].mat, cmap=ListedColormap(['w', 'k']))
 
     def update_func2d(frame, *fargs):
         global mat
@@ -43,9 +44,15 @@ else:
     ani = FuncAnimation(plt.gcf(), update_func2d, frames=simdata.frames, interval=500, repeat=True, repeat_delay=500, save_count=len(simdata.frames))
 
     ax = plt.gca()
-    minor_ticks_x = range(1, simdata.sim_size[0], 1)
-    minor_ticks_y = range(1, simdata.sim_size[1], 1)
+    minor_ticks_x = np.arange(0.5, simdata.sim_size[0] - 0.5, 1)
+    minor_ticks_y = np.arange(0.5, simdata.sim_size[1] - 0.5, 1)
     ax.set_xticks(minor_ticks_x, minor=True)
     ax.set_yticks(minor_ticks_y, minor=True)
     ax.grid(b=True, which='minor', color='gray', linestyle='-', linewidth=0.5)
+
+    for tic in ax.xaxis.get_major_ticks():
+        tic.tick1On = tic.tick2On = False
+    for tic in ax.yaxis.get_major_ticks():
+        tic.tick1On = tic.tick2On = False
+
     plt.show()
